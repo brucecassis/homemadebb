53e0e22236e9eff766d302a3d42a4b7f410e2856
brucecassis <lightinyourcar@gmail.com>
2025-11-15 01:50:42 +0100

    Remove ticker lists and comments from streamlit_app.py
    Removed multiple ticker lists and related comments from the Streamlit app.

--- streamlit_app.py
+++ streamlit_app.py
@@ -592,49 +592,6 @@ for idx, (name, ticker) in enumerate(commodities.items()):
 
 st.markdown('<div style="border-bottom: 1px solid #333; margin: 8px 0;"></div>', unsafe_allow_html=True)
 
-# ===== GRAPHIQUE MULTI-TICKERS COMPARATIF =====
-st.markdown("### 📈 COMPARATIVE CHART - PERFORMANCE %")
-
-# Liste COMPLÈTE des tickers S&P 500 (503 actions)
-sp500_tickers = ['A', 'AAL', 'AAPL', 'ABBV', 'ABC', 'ABMD', 'ABT', 'ACN', 'ADBE', 'ADI', 'ADM', 'ADP', 'ADSK', 'AEE', 'AEP', 'AES', 'AFL', 'AIG', 'AIZ', 'AJG', 'AKAM', 'ALB', 'ALGN', 'ALK', 'ALL', 'ALLE', 'AMAT', 'AMCR', 'AMD', 'AME', 'AMGN', 'AMP', 'AMT', 'AMZN', 'ANET', 'ANSS', 'AON', 'AOS', 'APA', 'APD', 'APH', 'APTV', 'ARE', 'ATO', 'AVB', 'AVGO', 'AVY', 'AWK', 'AXP', 'AZO', 'BA', 'BAC', 'BALL', 'BAX', 'BBWI', 'BBY', 'BDX', 'BEN', 'BF-B', 'BIIB', 'BIO', 'BK', 'BKNG', 'BKR', 'BLK', 'BMY', 'BR', 'BRK-B', 'BRO', 'BSX', 'BWA', 'BXP', 'C', 'CAG', 'CAH', 'CARR', 'CAT', 'CB', 'CBOE', 'CBRE', 'CCI', 'CCL', 'CDAY', 'CDNS', 'CDW', 'CE', 'CEG', 'CF', 'CFG', 'CHD', 'CHRW', 'CHTR', 'CI', 'CINF', 'CL', 'CLX', 'CMA', 'CMCSA', 'CME', 'CMG', 'CMI', 'CMS', 'CNC', 'CNP', 'COF', 'COO', 'COP', 'COST', 'CPB', 'CPRT', 'CPT', 'CRL', 'CRM', 'CSCO', 'CSGP', 'CSX', 'CTAS', 'CTLT', 'CTRA', 'CTSH', 'CTVA', 'CVS', 'CVX', 'CZR', 'D', 'DAL', 'DD', 'DE', 'DFS', 'DG', 'DGX', 'DHI', 'DHR', 'DIS', 'DLR', 'DLTR', 'DOV', 'DOW', 'DPZ', 'DRI', 'DTE', 'DUK', 'DVA', 'DVN', 'DXC', 'DXCM', 'EA', 'EBAY', 'ECL', 'ED', 'EFX', 'EIX', 'EL', 'EMN', 'EMR', 'ENPH', 'EOG', 'EPAM', 'EQIX', 'EQR', 'EQT', 'ES', 'ESS', 'ETN', 'ETR', 'ETSY', 'EVRG', 'EW', 'EXC', 'EXPD', 'EXPE', 'EXR', 'F', 'FANG', 'FAST', 'FBHS', 'FCX', 'FDS', 'FDX', 'FE', 'FFIV', 'FI', 'FICO', 'FIS', 'FITB', 'FLT', 'FMC', 'FOX', 'FOXA', 'FRC', 'FRT', 'FTNT', 'FTV', 'GD', 'GE', 'GILD', 'GIS', 'GL', 'GLW', 'GM', 'GNRC', 'GOOG', 'GOOGL', 'GPC', 'GPN', 'GRMN', 'GS', 'GWW', 'HAL', 'HAS', 'HBAN', 'HCA', 'HD', 'HES', 'HIG', 'HII', 'HLT', 'HOLX', 'HON', 'HPE', 'HPQ', 'HRL', 'HSIC', 'HST', 'HSY', 'HUM', 'HWM', 'IBM', 'ICE', 'IDXX', 'IEX', 'IFF', 'ILMN', 'INCY', 'INTC', 'INTU', 'INVH', 'IP', 'IPG', 'IQV', 'IR', 'IRM', 'ISRG', 'IT', 'ITW', 'IVZ', 'J', 'JBHT', 'JCI', 'JKHY', 'JNJ', 'JNPR', 'JPM', 'K', 'KDP', 'KEY', 'KEYS', 'KHC', 'KIM', 'KLAC', 'KMB', 'KMI', 'KMX', 'KO', 'KR', 'KVUE', 'L', 'LDOS', 'LEN', 'LH', 'LHX', 'LIN', 'LKQ', 'LLY', 'LMT', 'LNC', 'LNT', 'LOW', 'LRCX', 'LULU', 'LUV', 'LVS', 'LW', 'LYB', 'LYV', 'MA', 'MAA', 'MAR', 'MAS', 'MCD', 'MCHP', 'MCK', 'MCO', 'MDLZ', 'MDT', 'MET', 'META', 'MGM', 'MHK', 'MKC', 'MKTX', 'MLM', 'MMC', 'MMM', 'MNST', 'MO', 'MOH', 'MOS', 'MPC', 'MPWR', 'MRK', 'MRNA', 'MRO', 'MS', 'MSCI', 'MSFT', 'MSI', 'MTB', 'MTCH', 'MTD', 'MU', 'NCLH', 'NDAQ', 'NDSN', 'NEE', 'NEM', 'NFLX', 'NI', 'NKE', 'NOC', 'NOW', 'NRG', 'NSC', 'NTAP', 'NTRS', 'NUE', 'NVDA', 'NVR', 'NWS', 'NWSA', 'NXPI', 'O', 'ODFL', 'OKE', 'OMC', 'ON', 'ORCL', 'ORLY', 'OTIS', 'OXY', 'PANW', 'PARA', 'PAYC', 'PAYX', 'PCAR', 'PCG', 'PEAK', 'PEG', 'PEP', 'PFE', 'PFG', 'PG', 'PGR', 'PH', 'PHM', 'PKG', 'PKI', 'PLD', 'PM', 'PNC', 'PNR', 'PNW', 'PODD', 'POOL', 'PPG', 'PPL', 'PRU', 'PSA', 'PSX', 'PTC', 'PWR', 'PXD', 'PYPL', 'QCOM', 'QRVO', 'RCL', 'RE', 'REG', 'REGN', 'RF', 'RHI', 'RJF', 'RL', 'RMD', 'ROK', 'ROL', 'ROP', 'ROST', 'RSG', 'RTX', 'SBAC', 'SBNY', 'SBUX', 'SCHW', 'SHW', 'SIVB', 'SJM', 'SLB', 'SNA', 'SNPS', 'SO', 'SPG', 'SPGI', 'SRE', 'STE', 'STLD', 'STT', 'STX', 'STZ', 'SWK', 'SWKS', 'SYF', 'SYK', 'SYY', 'T', 'TAP', 'TDG', 'TDY', 'TECH', 'TEL', 'TER', 'TFC', 'TFX', 'TGT', 'TJX', 'TMO', 'TMUS', 'TPR', 'TRGP', 'TRMB', 'TROW', 'TRV', 'TSCO', 'TSLA', 'TSN', 'TT', 'TTWO', 'TXN', 'TXT', 'TYL', 'UAL', 'UDR', 'UHS', 'ULTA', 'UNH', 'UNP', 'UPS', 'URI', 'USB', 'V', 'VFC', 'VICI', 'VLO', 'VMC', 'VRSK', 'VRSN', 'VRTX', 'VTR', 'VTRS', 'VZ', 'WAB', 'WAT', 'WBA', 'WBD', 'WDC', 'WEC', 'WELL', 'WFC', 'WHR', 'WM', 'WMB', 'WMT', 'WRB', 'WRK', 'WST', 'WTW', 'WY', 'WYNN', 'XEL', 'XOM', 'XRAY', 'XYL', 'YUM', 'ZBH', 'ZBRA', 'ZION', 'ZTS']
-
-# NASDAQ 100 (100 actions tech)
-nasdaq_tickers = ['AAPL', 'ABNB', 'ADBE', 'ADI', 'ADP', 'ADSK', 'AEP', 'AMAT', 'AMD', 'AMGN', 'AMZN', 'ANSS', 'ASML', 'ATVI', 'AVGO', 'AZN', 'BIDU', 'BIIB', 'BKNG', 'CDNS', 'CEG', 'CHTR', 'CMCSA', 'COST', 'CPRT', 'CRWD', 'CSCO', 'CSGP', 'CSX', 'CTAS', 'CTSH', 'DDOG', 'DLTR', 'DXCM', 'EA', 'EBAY', 'ENPH', 'EXC', 'FANG', 'FAST', 'FISV', 'FTNT', 'GILD', 'GOOG', 'GOOGL', 'HON', 'IDXX', 'ILMN', 'INTC', 'INTU', 'ISRG', 'JD', 'KDP', 'KHC', 'KLAC', 'LCID', 'LRCX', 'LULU', 'MAR', 'MCHP', 'MDLZ', 'MELI', 'META', 'MNST', 'MRNA', 'MRVL', 'MSFT', 'MU', 'NFLX', 'NTES', 'NVDA', 'NXPI', 'ODFL', 'ON', 'ORLY', 'PANW', 'PAYX', 'PCAR', 'PDD', 'PEP', 'PYPL', 'QCOM', 'REGN', 'RIVN', 'ROST', 'SBUX', 'SGEN', 'SIRI', 'SNPS', 'TEAM', 'TMUS', 'TSLA', 'TXN', 'VRSK', 'VRTX', 'WBA', 'WBD', 'WDAY', 'XEL', 'ZM', 'ZS']
-
-# CAC 40 complet (40 actions)
-cac40_tickers = ['MC.PA', 'OR.PA', 'SAN.PA', 'AIR.PA', 'AI.PA', 'BNP.PA', 'CS.PA', 'SU.PA', 'TTE.PA', 'BN.PA', 'CA.PA', 'ACA.PA', 'SGO.PA', 'GLE.PA', 'VIE.PA', 'DSY.PA', 'EN.PA', 'CAP.PA', 'DG.PA', 'RMS.PA', 'SAF.PA', 'ORA.PA', 'PUB.PA', 'KER.PA', 'URW.PA', 'RI.PA', 'ML.PA', 'STM.PA', 'TEP.PA', 'VIV.PA', 'SOP.PA', 'WLN.PA', 'BOL.PA', 'ERF.PA', 'EL.PA', 'BVI.PA', 'GET.PA', 'FP.PA', 'LR.PA', 'STLA.PA']
-
-# SIX Swiss SMI (20 actions principales)
-six_tickers = ['NESN.SW', 'ROG.SW', 'NOVN.SW', 'UBSG.SW', 'ABBN.SW', 'ZURN.SW', 'SREN.SW', 'GIVN.SW', 'CSGN.SW', 'SLHN.SW', 'LOGN.SW', 'SIKA.SW', 'GEBN.SW', 'SCMN.SW', 'ALC.SW', 'LONN.SW', 'CFR.SW', 'PGHN.SW', 'HOLN.SW', 'VATN.SW']
-
-# Combiner toutes les listes
-all_tickers = ['--- CRYPTO ---', 'BTC-USD', 'ETH-USD', 'SOL-USD', 'BNB-USD', 'XRP-USD', 'ADA-USD', 'DOGE-USD', 'MATIC-USD', 'DOT-USD', 'AVAX-USD',
-               '--- NASDAQ 100 ---'] + sorted(list(set(nasdaq_tickers))) + \
-              ['--- S&P 500 ---'] + sorted(list(set(sp500_tickers))) + \
-              ['--- CAC 40 ---'] + sorted(cac40_tickers) + \
-              ['--- SIX SWISS ---'] + sorted(six_tickers)
-
-col_chart1, col_chart2, col_chart3 = st.columns([3, 1, 1])
-
-with col_chart1:
-    # Input texte pour contourner la limite du multiselect
-    tickers_input = st.text_input(
-        "Entrez les tickers séparés par des virgules (ex: AAPL, MSFT, TSLA, MC.PA, NESN.SW)",
-        value="AAPL, MSFT, GOOGL",
-        help="Max 10 tickers | S&P500: AAPL, MSFT | NASDAQ: NVDA, AMD | CAC40: MC.PA, OR.PA | SIX: NESN.SW, ROG.SW | Crypto: BTC-USD, ETH-USD"
-    )
-    # Convertir en liste et nettoyer
-    selected_tickers = [t.strip().upper() for t in tickers_input.split(',') if t.strip()]
-    
-    # Expander avec liste complète pour référence
-    with st.expander("📋 Voir tous les tickers disponibles"):
-        st.markdown("**🔷 CRYPTO:** BTC-USD, ETH-USD, SOL-USD, BNB-USD, XRP-USD, ADA-USD")
-        st.markdown("**🔷 NASDAQ 100:** AAPL, MSFT, GOOGL, META, NVDA, TSLA, AMD, INTC, NFLX, AMZN...")
-        st.markdown("**🔷 S&P 500:** Toutes les actions US (503 actions)")
-        st.markdown("**🔷 CAC 40:** MC.PA (LVMH), OR.PA (L'Oréal), SAN.PA (Sanofi), AIR.PA (Airbus)...")
-        st.markdown("**🔷 SIX Swiss:** NESN.SW (Nestlé), ROG.SW (Roche), NOVN.SW (Novartis)...")
-        st.caption("Pour voir la liste complète des tickers, visitez : finance.yahoo.com")
-col_main, col_sidebar = st.columns([2.5, 1])
 
 with col_main:
     st.markdown("### 📰 MARKET NEWS")
